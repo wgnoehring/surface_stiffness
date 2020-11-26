690830e7f69ab89869902590319e5d87ef0d2602
Wolfram Georg Nöhring <wnoehring@o2.box>
2020-11-26 18:37:07 +0100

    Moved method and implemented bootstrap of stiffness
    * Moved calculate_stiffness to the matrix module,
      so that it can be imported
    * Some modifications in convenience script for calculating
      surface stiffness; prepared implementation of bootstrap
      sampling

--- scripts/calculate_stiffness_from_greens_function_average.py
+++ scripts/calculate_stiffness_from_greens_function_average.py
@@ -10,9 +10,71 @@ from surface_stiffness.matrix import (
     calculate_blockwise_inverse,
     load_atomistic_stiffness,
     invert_grid_of_flattened_matrices,
+    calculate_stiffness
 )
 
 def main():
+    args = parse_command_line()
+    if args.confidence_interval != "stddev":
+        num_stddev = 0
+    else:
+        num_stddev = args.num_stddev
+    if args.confidence_interval == "bootstrap":
+        raise NotImplementedError
+    # The average stiffness will be calculated in any case.
+    greens_functions = np.load(args.greens_functions)
+    degrees_of_freedom = 3
+    num_atoms_edge = int(np.sqrt(greens_functions.shape[0] // degrees_of_freedom))
+    # The number of subsurface planes and the surface
+    # width are irrelevant here and can be set to 1
+    config = configurations.Configuration(
+        material=None, crystal=configurations.FCCSurface001(num_atoms_edge, 1, 1.0),
+    )
+    mean_stiff, upper_stiff, lower_stiff = calculate_stiffness(
+        greens_functions, config, num_stddev=num_stddev, mask=None
+    )
+    print(mean_stiff.shape)
+    filename = f"./stiffness_from_average_of_greens_functions.npy"
+    print(f"...writing average stiffnesses to {filename}")
+    np.save(filename, np.ma.filled(mean_stiff))
+
+    if args.confidence_interval == "stddev":
+        filename = f"./stiffness_from_average_of_greens_functions_confidence_interval_plus_{args.confidence_interval}_stddev.npy"
+        print(f"...writing upper confidence limit of stiffness to {filename}")
+        np.save(filename, np.ma.filled(upper_stiff))
+        filename = f"./stiffness_from_average_of_greens_functions_confidence_interval_minus_{args.confidence_interval}_stddev.npy"
+        print(f"...writing lower confidence limit of stiffness to {filename}")
+        np.save(filename, np.ma.filled(lower_stiff))
+
+    if args.element_wise is not None:
+        mask_for_symbol, symbols = config.crystal.create_symbol_masks_for_surface(
+            args.element_wise, return_symbols=True,
+        )
+        unique_symbols = np.unique(symbols)
+        for symbol in unique_symbols:
+            sutf = symbol.decode("UTF-8")
+            print(f"Calculating stiffness for sites with {sutf} atoms")
+            mask = mask_for_symbol[symbol].ravel()
+            num_masked = len(np.nonzero(mask)[0])
+            print(
+                f"{num_masked} masked values --> {config.crystal.num_atoms_surface-num_masked} not masked"
+            )
+            mean_stiff, upper_stiff, lower_stiff = calculate_stiffness(
+                greens_functions, config, num_stddev=num_stddev, mask=mask
+            )
+            filename = f"./stiffness_from_average_of_greens_functions_{sutf}_atoms.npy"
+            print(f"...writing average stiffnesses of {sutf} atoms to {filename}")
+            np.save(filename, np.ma.filled(mean_stiff))
+            if args.confidence_interval == "stddev":
+                filename = f"./stiffness_from_average_of_greens_functions_{sutf}_atoms_confidence_interval_plus_{args.confidence_interval}_stddev.npy"
+                print(f"...writing upper confidence limit of stiffnesses of {sutf} atoms to {filename}")
+                np.save(filename, np.ma.filled(upper_stiff))
+                filename = f"./stiffness_from_average_of_greens_functions_{sutf}_atoms_confidence_interval_minus_{args.confidence_interval}_stddev.npy"
+                print(f"...writing lower confidence limit of stiffnesses of {sutf} atoms to {filename}")
+                np.save(filename, np.ma.filled(lower_stiff))
+
+
+def parse_command_line():
     parser = argparse.ArgumentParser(
         description=dedent("""\
             Invert elastic greens functions to obtain stiffnesses.
@@ -58,172 +120,61 @@ def main():
             )
         ),
     )
-    parser.add_argument(
-        "-c",
-        "--confidence-interval",
-        type=int,
-        default=0,
+    # Arguments for particular confidence interval types
+    subparsers = parser.add_subparsers(
+        dest="confidence_interval",
         help=(dedent("""\
-            Calculate bounds by adding/substracting multiples of the
-            standard deviations of the real and imaginary parts of the
-            greens functions before inversion.
+            Method for calculating confidence intervals. If none is
+            specified, then only the mean stiffness will be calculated.
+            """
+            )
+        )
+    )
+    stddev = subparsers.add_parser(
+        "stddev",
+        help=(dedent("""\
+            Calculate confidence intervals by adding/substracting
+            multiples of the standard deviations of the real and
+            imaginary parts of the greens functions before inversion.
+            """
+            )
+        )
+    )
+    bootstrap = subparsers.add_parser(
+        "bootstrap",
+        help=(dedent("""\
+            Calculate confidence intervals using a bootstrap approach. At
+            every point in the Brillouin zone, and for every component of
+            the Green's function, resample the component num_bootstrap
+            times. Calculate the average of each sample and invert. Finally,
+            calculate the specified percentiles (argument --percentiles).
             """
             )
         ),
     )
+    stddev.add_argument(
+        "-n",
+        "--num_stddev",
+        type=int,
+        default=0,
+        help="Number of standard deviations around the mean"
+    )
+    bootstrap.add_argument(
+        "-n", 
+        "--num_resamples", 
+        type=int,
+        default=100,
+        help="Number of bootstrap resamples"
+    )
+    bootstrap.add_argument(
+        "-p", 
+        "--percentiles", 
+        type=float,
+        nargs="+",
+        help="Percentiles to calculate, e.g. 10 90"
+    )
     args = parser.parse_args()
-    greens_functions = np.load(args.greens_functions)
-    degrees_of_freedom = 3
-    num_atoms_edge = int(np.sqrt(greens_functions.shape[0] // degrees_of_freedom))
-    # The number of subsurface planes and the surface
-    # width are irrelevant here and can be set to 1
-    config = configurations.Configuration(
-        material=None, crystal=configurations.FCCSurface001(num_atoms_edge, 1, 1.0),
-    )
-    mean_stiff, upper_stiff, lower_stiff = calculate_stiffness(
-        greens_functions, config, num_stddev=args.confidence_interval, mask=None
-    )
-    filename = f"./stiffness_from_average_of_greens_functions.npy"
-    print(f"...writing average stiffnesses to {filename}")
-    np.save(filename, np.ma.filled(mean_stiff))
-    if args.confidence_interval > 0:
-        filename = f"./stiffness_from_average_of_greens_functions_confidence_interval_plus_{args.confidence_interval}_stddev.npy"
-        print(f"...writing upper confidence limit of stiffness to {filename}")
-        np.save(filename, np.ma.filled(upper_stiff))
-        filename = f"./stiffness_from_average_of_greens_functions_confidence_interval_minus_{args.confidence_interval}_stddev.npy"
-        print(f"...writing lower confidence limit of stiffness to {filename}")
-        np.save(filename, np.ma.filled(lower_stiff))
-
-    if args.element_wise is not None:
-        mask_for_symbol, symbols = config.crystal.create_symbol_masks_for_surface(
-            args.element_wise, return_symbols=True,
-        )
-        unique_symbols = np.unique(symbols)
-        for symbol in unique_symbols:
-            sutf = symbol.decode("UTF-8")
-            print(f"Calculating stiffness for sites with {sutf} atoms")
-            mask = mask_for_symbol[symbol].ravel()
-            num_masked = len(np.nonzero(mask)[0])
-            print(
-                f"{num_masked} masked values --> {config.crystal.num_atoms_surface-num_masked} not masked"
-            )
-            mean_stiff, upper_stiff, lower_stiff = calculate_stiffness(
-                greens_functions, config, num_stddev=args.confidence_interval, mask=mask
-            )
-            filename = f"./stiffness_from_average_of_greens_functions_{sutf}_atoms.npy"
-            print(f"...writing average stiffnesses of {sutf} atoms to {filename}")
-            np.save(filename, np.ma.filled(mean_stiff))
-            if args.confidence_interval is not None:
-                filename = f"./stiffness_from_average_of_greens_functions_{sutf}_atoms_confidence_interval_plus_{args.confidence_interval}_stddev.npy"
-                print(f"...writing upper confidence limit of stiffnesses of {sutf} atoms to {filename}")
-                np.save(filename, np.ma.filled(upper_stiff))
-                filename = f"./stiffness_from_average_of_greens_functions_{sutf}_atoms_confidence_interval_minus_{args.confidence_interval}_stddev.npy"
-                print(f"...writing lower confidence limit of stiffnesses of {sutf} atoms to {filename}")
-                np.save(filename, np.ma.filled(lower_stiff))
-
-
-def calculate_stiffness(greens_functions, config, num_stddev=0, mask=None):
-    r"""Calculate stiffness by inversion of elastic Greens functions
-
-    We assume that the atoms are arranged in a two-dimensional simple cubic
-    lattice. Let :math:`N` be the number of atoms along the edge. The input
-    array :code:`greens_functions` contains the elastic Greens functions.
-    Each :math:`3\times{}3` block contains the Greens functions of one pair
-    of sites and, corresponding to one point in the Brillouin zone. Holding
-    one site fixed, we can obtain data for all points in the Brillouin zone
-    by varying the other site. Varying the first site, we obtain :math:`N^2`
-    :math:`3\times{}3` matrices for each Brillouin zone point. This function
-    first calculates the average at each Brillouin zone point and then inverts
-    the :math:`3\times{}3` matrices to obtain stiffness matrices.
-
-    The :math:`N^2` :math:`3\times{}3` matrices at a given Brillouin zone
-    point take the same values in case of a pure crytal. In the case of 
-    an alloy (or some other disorder), the values differ. We can compute
-    a confidence interval of the variation in stiffness by adding and 
-    subtracting a multiple of the standard deviation of the Green's 
-    functions before inversion. Since the Greens functions are complex-valued,
-    we use the following approach. Noting that the variance of a complex
-    number is :math:`Var[z] = Var[Re(z)] + Var[Im(z)]`, where :math:`Re`
-    and :math:`Im` indicate the real and imaginary parts, respectively, 
-    we compute :math:`z +- m(\sigma(Re(z)) + i * \sigma(Im(z)))`, where
-    :math:`\sigma` is the standard deviation, and :math:`m` is the number
-    of standard deviations to add.
-
-    Parameters
-    ----------
-    greens_functions: array-like
-        Array with shape :code:`((N*N*3), (N*N*3))`, where each block of shape
-        :code:`(3,3)` contains the Greens functions for one atomic site.
-    config: surface_stiffness.configurations.Configuration
-        Configuration of crystal and material, which provides the information
-        of how to reshape a row in greens_functions to obtain an array of shape
-        :code:`(N,N)`, whose components are arranged according to the order of
-        the corresponding sites in space, or the corresponding wavevectors in
-        reciprocal space.
-    num_stddev: int
-        Number of standard deviations to add/subtract from the average Greens
-        functions to obtain a confidence interval.
-    mask: array-like
-        Array of ones and zeros, or None. If None, then the average of the 
-        Greens functions runs over all sites. If not None, then the average
-        runs only over sites with a one in :code:`mask`.
-
-    Returns
-    -------
-    mean_stiff: numpy.ndarray
-        :code:`((N*3), (N*3))` array containing the inverses of the
-        average Greens functions. Each block of shape :code:`(3,3)`
-        the inverse of the corresponding block in the :code:`((N*3),(N*3))` 
-        of average Greens functions.
-    upper_stiff: numpy.ndarray
-        Upper confidence limit, array with the same shape as :code:`mean_stiff`
-    lower_stiff: numpy.ndarray
-        Lower confidence limit, array with the same shape as :code:`mean_stiff`
-
-    """
-    if num_stddev < 0:
-        raise ValueError("number of standard deviations for confidence interval must be greater or equal to zero")
-    mean_both, = load_atomistic_stiffness(
-        greens_functions,
-        reshape=config.crystal.reshape,
-        statistics=[np.average],
-        part="both",
-        indexing="matrix",
-        mask=mask,
-    )
-    mean_stiff = invert_grid_of_flattened_matrices(mean_both)
-    upper_stiff = lower_stiff = None
-    if num_stddev > 0:
-        std_both, = load_atomistic_stiffness(
-            greens_functions,
-            reshape=config.crystal.reshape,
-            statistics=[np.std],
-            part="both",
-            indexing="matrix",
-            mask=mask,
-        )
-        mean_real, std_real = load_atomistic_stiffness(
-            greens_functions,
-            reshape=config.crystal.reshape,
-            statistics=[np.average, np.std],
-            part="real",
-            indexing="matrix",
-            mask=mask,
-        )
-        mean_imag, std_imag = load_atomistic_stiffness(
-            greens_functions,
-            reshape=config.crystal.reshape,
-            statistics=[np.average, np.std],
-            part="imag",
-            indexing="matrix",
-            mask=mask,
-        )
-        fluctuation = std_real + 1.0j * std_imag
-        upper = mean_both + num_stddev * fluctuation
-        lower = mean_both - num_stddev * fluctuation
-        upper_stiff = invert_grid_of_flattened_matrices(upper)
-        lower_stiff = invert_grid_of_flattened_matrices(lower)
-    return mean_stiff, upper_stiff, lower_stiff
+    return args
 
 
 if __name__ == "__main__":
