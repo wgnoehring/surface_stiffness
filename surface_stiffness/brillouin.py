fa840b0461a92a72fe0e161a2c235567371a78a4
wnoehring <wolfram.noehring@imtek.uni-freiburg.de>
2021-10-20 15:14:56 +0200

    Method for extracting path from rectangular zone

--- surface_stiffness/brillouin.py
+++ surface_stiffness/brillouin.py
@@ -140,6 +140,117 @@ def extract_path(values, x_grid, y_grid, fraction=1.0, true_edge_lengths=None):
     return path
 
 
+def extract_path_rect(values, x_grid, y_grid, fraction=1.0, true_edge_lengths=None):
+    """Extract values on a path through a quadrant of the Brillouin zone.
+
+    Extract the values on the path
+    :math:`\\bar{\Gamma}`-:math:`\\bar{X}`-:math:`\\bar{M}`-:math:`\\bar{\Gamma}`,
+    or a scaled version of this path. The path connects the points
+    :math:`\\bar{\Gamma}`, :math:`\\bar{X}`, and :math:`\\bar{M}`, where
+    :math:`\\bar{\Gamma}` is the origin of the Brillouin zone (zero
+    wavector), :math:`\\bar{X}` is the midpoint of the right edge
+    (or the lower right corner of the upper right quadrant), and
+    :math:`\\bar{M}` is the upper right corner. The path is closed.
+
+    If :math:`n` is the number of rows or columns, then the path
+    consists of :math:`3n-3` unique points. We add the starting point
+    :math:`\\bar{\Gamma}` at the end, so the returned path has :math:`3n-2` points.
+
+    When determining the path coordinate, we need to be careful because the
+    values are associated with pixels. Their coordinates are the coordinates of
+    the centroid of the pixel. It follows that the values at the boundary of the
+    Brillouin zone are not available. The boundary in a direction corresponds
+    to the edge of the last pixel in that direction, not its centroid. For
+    example, the points of the segment :math:`\\bar{\Gamma}`-:math:`\\bar{X}`
+    have x-coordinates in :math:`[0, l_x)`, where :math:`l_x` is the
+    length of the Brillouin zone in x-direction. The offset for the
+    segment :math:`\\bar{X}`-:math:`\\bar{M}` is :math:`l_x`, however.
+
+    Parameters
+    ----------
+    values: numpy.ndarray
+        A quadrant of the Brillouin zone as a square array. The lower
+        left corner of the array (index= -1, 0) should be the origin.
+        The x-coordinate should increase with increasing column number,
+        and the y-coordinate should increase with decreasing row number.
+    x_grid: numpy.ndarray
+        x-coordinates associated with values
+    y_grid: numpy.ndarray
+        y-coordinates
+    fraction: float
+        Fraction of the quadrant of the Brillouin zone that is
+        traversed. If fraction < 1.0, then the path is shrunk so that
+        the endpoints are at a distance lx*fraction and ly*fraction
+        from the origin. Only pixels within this zone are considered.
+
+    Returns
+    -------
+    path: numpy.ndarray
+        Array with shape :code:`(N, 4)`, where :code:`N` is the number
+        of points of the path. The first two columns are the x- and
+        y-coordinates of the points, the third column contains the
+        associated values from :code:`values`, and the fourth colum
+        is a monotonically increasing coordinate along the path.
+
+    Examples
+    --------
+    >>> component = np.real(stiffness[:, :, voigt_index])
+    >>> xx, yy = generate_wavevectors(*component.shape)
+    >>> imid = component.shape[0] // 2
+    >>> quadrant = component[imid:, imid:]
+    >>> xx_quadrant = xx[imid:, imid:]
+    >>> yy_quadrant = yy[imid:, imid:]
+    >>> path = extract_path(
+    >>>     np.flipud(quadrant),
+    >>>     xx_quadrant,
+    >>>     np.flipud(yy_quadrant),
+    >>>     fraction=1.0
+    >>> )
+    """
+    assert x_grid.shape[0] == y_grid.shape[0]
+    assert x_grid.shape[1] == y_grid.shape[1]
+    ny, nx = x_grid.shape
+    dx = x_grid[0, 1] - x_grid[0, 0]
+    dy = y_grid[-2, 0] - y_grid[-1, 0]
+    if true_edge_lengths is None:
+        true_edge_length_x = x_grid[0, -1] + 0.5 * dx
+        true_edge_length_y = y_grid[0, 0] + 0.5 * dy
+    else:
+        true_edge_length_x, true_edge_length_y = true_edge_lengths
+    if fraction == 1.0:
+        path = np.zeros((nx + ny - 1, 4), dtype=values.dtype) 
+        # bottom row
+        s = np.full(shape=nx, fill_value=ny - 1), np.arange(0, nx, 1, dtype=int)
+        grids = (x_grid, y_grid, values)
+        for i, grid in zip(range(3), grids):
+            path[:nx, i] = grid[s]
+        # rightmost column
+        s = np.arange(ny - 2, -1, -1, dtype=int), np.full(ny - 1, fill_value=nx - 1)
+        for i, grid in zip(range(3), grids):
+            path[nx : nx + ny - 1, i] = grid[s]
+        # Coordinate along path
+        path[:nx, 3] = path[:nx, 0]
+        path[nx : nx + ny - 1, 3] = path[nx : nx + ny - 1, 1] + true_edge_length_x
+    else:
+        rows, cols = np.where(
+            (x_grid <= fraction * true_edge_length_x)
+            & (y_grid <= fraction * true_edge_length_y)
+        )
+        rslice = slice(rows[0], rows[-1] + 1)
+        cslice = slice(cols[0], cols[-1] + 1)
+        path = extract_path_rect(
+            values[rslice, cslice],
+            x_grid[rslice, cslice],
+            y_grid[rslice, cslice],
+            fraction=1.0,
+            true_edge_lengths=(
+                fraction * true_edge_length_x,
+                fraction * true_edge_length_y,
+            ),
+        )
+    return path
+
+
 def generate_wavevectors(nrows, ncols):
     """Generate wavevectors.
 
